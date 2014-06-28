4b66317a30c0f057733e0564ed64be9b5a736689
spmno <sunqingpeng@hotmail.com>
2014-06-28 22:33:57 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeMonkeyServer::Application.config.secret_key_base = '8aa808f70e27f9afd145406d25e8dfa27f07791253a0a5a3cf875757febb3d74eddb0e3770b2114ee35faac042f90203e3234af67f93e27300d40b57a2ea2bf2'
