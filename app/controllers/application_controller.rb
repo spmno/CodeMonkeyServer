5d44f76a0024e08b9653916bf1b2e497ce7ecb80
spmno <sunqingpeng@hotmail.com>
2014-09-28 22:08:02 +0800

    complete authentication for json.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # This is our new function that comes before Devise's one
-  before_filter :authenticate_user_from_token!
+  #before_filter :authenticate_user_from_token!
   # This is Devise's authentication
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   private
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,31 +1,48 @@
 class Users::SessionsController < Devise::SessionsController
   prepend_before_filter :require_no_authentication, :only => [:create]
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-  before_filter :validate_auth_token, :except => [:create, :new]
+  before_filter :validate_auth_token, :except => [:create], :if => Proc.new { |c| c.request.format == 'application/json' }
   include Devise::Controllers::Helpers
   include ApiHelper
-  respond_to :json
 
   def create
-    resource = User.find_for_database_authentication(:email => params[:user][:email])
-    if !resource
-      user = User.find_by_email(params[:user][:email])
-      if user.authenticate(params[:user][:password])
+=begin
+    if params[:format] == "json"
+      data_json=JSON.parse request.body.read
+      email = data_json["user"]["email"]
+      password = data_json["user"]["password"]
+      user = User.find_by_email(email)
+      if user.authenticate(password)
         respond_with user.authentication_token.as_json
+        sign_in(:user, resource)
+      end
+    end
+=end
+    resource = User.find_for_database_authentication(:email => params[:user][:email])
+
+    if resource.nil?
+      respond_to do |format|
+        format.html { Error with your login or password }
+        format.json { render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401 }
       end
     end
 
     if resource.valid_password?(params[:user][:password])
-      sign_in(:user, resource)
-      resource.ensure_authentication_token!
-      render :json=> {:success => true, :token => resource.authentication_token}
-      return
+      sign_in("user", resource)
+      respond_to do |format|
+        format.html { respond_with resource, location: after_sign_in_path_for(resource) }
+        format.json { render :json=> {:success=>true, :auth_token=>resource.authentication_token, :email=>resource.email} }
+      end
+    else
+      respond_to do |format|
+        format.html { Error with your login or password }
+        format.json { render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401 }
+      end
     end
-    failure
   end
 
   def destroy
-    resource.reset_authentication_token!
+    #resource.reset_authentication_token!
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     render :status => 200, :json => {}
   end
